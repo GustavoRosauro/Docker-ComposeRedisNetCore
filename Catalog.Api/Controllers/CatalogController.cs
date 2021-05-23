02799f68b0eed9f64344d92b851ccb7ff7cbbfb0
GustavoRosauro <gustavosrosauro@gmail.com>
2021-05-23 08:29:39 -0300

    Redis docker-compose

---
+++ Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Product>> ListaProdutos()
+        {
+            return Ok(new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "048509438043",
+                    Name = "Golf",
+                    Category = "Volksvagem",
+                    Description ="Good Car",
+                    Image = "carro.png",
+                    Price = 120003.43f
+                },
+                new Product()
+                {
+                    Id = "048509438043",
+                    Name = "Pajero",
+                    Category = "Mitsubich",
+                    Description ="Good Car",
+                    Image = "carro2.png",
+                    Price = 200020.43f
+                }
+            });
+        }
+    }
+}
---
+++ RedisApis/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using BasketApi.Entities;
+using BasketApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet("{userName}", Name = "GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket?? new ShoppingCart(userName));
+        }
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        public async Task<ActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
